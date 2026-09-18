03ae62745b8d00a1f2c0b2ccbb55f8117558a244
Riya <iamriyasaryam@gmail.com>
2026-09-18 23:41:09 +0530

    Day 3 Project phase 4 done

--- Expensus/script.js
+++ Expensus/script.js
@@ -2,19 +2,56 @@ const form = document.querySelector("#expense-form")
 
 let expenseName = document.querySelector("#expense_name")
 let expenseAmount = document.querySelector("#expense_amount")
+let expenseList = document.querySelector(".expense-list")
+
+const expenses = [];
 
 form.addEventListener("submit", (event) => {
       event.preventDefault();
      
-      console.log(expenseName.value);
-      console.log(expenseAmount.value);
-      console.log(typeof(expenseAmount));
-
+      let name = expenseName.value;
       const amount = Number(expenseAmount.value)
-      console.log(amount);
-      console.log(typeof(amount));
       
+       if (name === "") {
+            console.log("Expense name is required");
+            return;
+      }
+
+      if (!Number.isFinite(amount) || amount <= 0) {
+            console.log("Enter a valid amount");
+            return;
+      }
+
+      const expense = {
+            name: name,
+            amount: amount,
+            date : new Date()
+      };
+
+      expenses.push(expense)
+      renderExpenses();
+      form.reset();
+      
+      
+
       
       
 });
+
+function renderExpenses() {
+      expenseList.innerHTML = "";
+      expenses.forEach(
+            function(expense) {
+             const expenseItem = document.createElement("div");
+
+             expenseItem.innerHTML = `
+             <h3>${expense.name}</h3>
+             <p>₹${expense.amount}</p>
+            `;
+
+            expenseList.appendChild(expenseItem);
+
+            }
+      );
+}
 
